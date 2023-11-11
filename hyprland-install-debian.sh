267f47d0b3f7d04b01914d995a23e6435f59ff73
xioren <40043405+xioren@users.noreply.github.com>
2023-11-10 20:17:21 -0800

    Add files via upload

---
+++ hyprland-install-debian.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+
+HYPRLAND_VERSION = "0.27.0"
+CONFIG_PATH = ~/.config/hypr/
+
+
+# initial setup
+mkdir hyprland && cd hyprland
+sudo apt install wget git
+wget https://github.com/hyprwm/Hyprland/releases/download/v$HYPRLAND_VERSION/v$HYPRLAND_VERSION.tar.gz
+tar -xf v$HYPRLAND_VERSION.tar.gz
+
+
+# install dependencies
+sudo apt install -y \
+autoconf \
+build-essential \
+fontconfig \
+gettext \
+gettext-base \
+glslang-tools \
+kitty \
+libavcodec-dev \
+libavformat-dev \
+libavutil-dev \
+libdisplay-info-dev \
+libdrm-dev \
+libegl-dev \
+libegl1-mesa-dev \
+libffi-dev \
+libfontconfig-dev \
+libgles2 \
+libinput-bin \
+libinput-dev \
+libliftoff-dev \
+libpixman-1-dev \
+libseat-dev \
+libtool \
+libudev-dev \
+libvulkan-dev \
+libwayland-dev \
+libxcb-composite0-dev \
+libxcb-dri3-dev \
+libxcb-ewmh2 \
+libxcb-ewmh-dev \
+libxcb-present-dev \
+libxcb-icccm4-dev \
+libxcb-render-util0-dev \
+libxcb-res0-dev \
+libxcb-xinput-dev \
+libxkbcommon-x11-dev \
+libxkbregistry-dev \
+libxkbcommon-dev \
+libxml2-dev \
+make \
+seatd \
+xcb-proto \
+xdg-desktop-portal-wlr \
+xutils-dev \
+xwayland
+
+
+# build and install libxcb-errors
+git clone https://gitlab.freedesktop.org/xorg/lib/libxcb-errors && cd libxcb-errors
+git submodule update --init
+bash autogen.sh
+make
+sudo make install
+sudo ldconfig
+
+
+# install hyprland
+cd hyprland
+sudo mv hyprctl Hyprland /usr/bin/
+sudo mv libwlroots.so.XX032 /usr/lib/
+sudo mv example/hyprland.desktop /usr/share/wayland-sessions/
+mkdir -p $CONFIG_PATH && mv example/hyprland.conf $CONFIG_PATH
+
+
+echo "hyprland installed"
