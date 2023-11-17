eb442413362cb61f9df3342413dea706a1b9fab8
xioren <40043405+xioren@users.noreply.github.com>
2023-11-17 09:21:48 -0800

    Update hyprland-install-debian.sh

--- hyprland-install-debian.sh
+++ hyprland-install-debian.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# highest version compatible with Debian 12/testing
 HYPRLAND_VERSION="0.27.0"
 CONFIG_PATH=~/.config/hypr/
 
@@ -11,7 +12,7 @@ mkdir hyprland-tmp && cd hyprland-tmp
 
 
 # install dependencies
-sudo apt install -y \
+sudo apt update && sudo apt install -y \
 autoconf \
 build-essential \
 fontconfig \
@@ -53,10 +54,14 @@ libxkbregistry-dev \
 libxkbcommon-dev \
 libxml2-dev \
 make \
+polkit-kde-agent-1 \
 seatd \
+qtwayland5 \
+qt6-wayland \
 wget \
+xdg-desktop-portal \
+xdg-desktop-portal-gtk \
 xcb-proto \
-xdg-desktop-portal-wlr \
 xutils-dev \
 xwayland
 
@@ -69,6 +74,28 @@ make && sudo make install
 sudo ldconfig
 cd ..
 
+
+# select xdg-desktop backend
+echo "please select a xdg-desktop-portal backend"
+echo "1: xdg-desktop-portal-wlr"
+echo "2: xdg-desktop-portal-hyprland"
+read -p "(1 or 2): " choice
+
+if [ "$choice" == "1" ]; then
+        sudo apt install -y xdg-desktop-portal-wlr
+elif  [ "$choice" == "2" ]; then
+        sudo apt install -y cmake ninja-build libsystemd-dev libgbm-dev libpipewire-0.3-dev libspa-0.2-dev
+        git clone --recursive "https://github.com/hyprwm/xdg-desktop-portal-hyprland"
+        cd xdg-desktop-portal-hyprland
+        make all && sudo make install
+        cd ..
+else
+        # IDEA: could default to 1 instead of exit
+        echo "invalid choice"
+        exit 1
+fi
+
+
 # install hyprland
 wget https://github.com/hyprwm/Hyprland/releases/download/v$HYPRLAND_VERSION/v$HYPRLAND_VERSION.tar.gz
 tar -xf v$HYPRLAND_VERSION.tar.gz && cd hyprland/
@@ -79,5 +106,66 @@ sudo mv example/hyprland.desktop /usr/share/wayland-sessions/
 mkdir -p $CONFIG_PATH
 mv example/hyprland.conf $CONFIG_PATH
 
+
+# install extras
+# TODO: add more options
+
+echo "install utilities?"
+echo "1: all"
+echo "2: choose"
+echo "3: none"
+read -p "(1, 2, or 3): " choice
+
+if [ "$choice" == "1" ]; then
+        # all
+        sudo apt install -y \
+        libmagic1 \   
+        libmagic-dev \
+        mako-notifier \
+        rofi \
+        waybar
+        git clone https://github.com/hyprwm/hyprpaper && cd hyprpaper
+        make all && sudo make install
+        cd ..
+elif  [ "$choice" == "2" ]; then
+        # choose
+
+        # hyprpaper 
+        echo "install hyprpaper wallpaper utility?"
+        read -p "(y or n): " choice
+
+        if [ "$choice" == "y" ]; then
+                sudo apt install -y libmagic1 libmagic-dev
+                git clone https://github.com/hyprwm/hyprpaper && cd hyprpaper
+                make all && sudo make install
+                cd ..
+        fi
+
+        # mako
+        echo "install mako notifier?"
+        read -p "(y or n): " choice
+
+        if [ "$choice" == "y" ]; then
+                sudo apt install -y mako-notifier
+        fi
+
+        # rofi
+        echo "install rofi app launcher?"
+        read -p "(y or n): " choice
+
+        if [ "$choice" == "y" ]; then
+                sudo apt install -y rofi
+        fi
+
+        # waybar
+        echo "install waybar status bar?"
+        read -p "(y or n): " choice
+
+        if [ "$choice" == "y" ]; then
+                sudo apt install -y waybar
+        fi
+fi
+
+
 # remove temp dir?
-echo "hyprland installed"
+echo "installation complete"
