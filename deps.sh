c00c6fb81ac1d46ce6c53b871bd643150ba53611
xioren <40043405+xioren@users.noreply.github.com>
2024-05-27 22:47:37 -0700

    Create deps.sh

---
+++ deps.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y \
+autoconf \
+automake \
+build-essential \
+cmake \
+fontconfig \
+gettext \
+gettext-base \
+git \
+glslang-tools \
+kitty \
+libavcodec-dev \
+libavformat-dev \
+libavutil-dev \
+libdisplay-info-dev \
+libdrm-dev \
+libegl-dev \
+libegl1-mesa-dev \
+libffi-dev \
+libfontconfig-dev \
+libgles2 \
+libinput-bin \
+libinput-dev \
+libliftoff-dev \
+libpixman-1-dev \
+libseat-dev \
+libtool \
+libudev-dev \
+libvulkan-dev \
+libwayland-dev \
+libxcb-composite0-dev \
+libxcb-dri3-dev \
+libxcb-ewmh2 \
+libxcb-ewmh-dev \
+libxcb-present-dev \
+libxcb-icccm4-dev \
+libxcb-render-util0-dev \
+libxcb-res0-dev \
+libxcb-xinput-dev \
+libxkbcommon-x11-dev \
+libxkbregistry-dev \
+libxkbcommon-dev \
+libxml2-dev \
+make \
+seatd \
+qtwayland5 \
+qt6-wayland \
+wayland-protocols \
+wget \
+xdg-desktop-portal \
+xdg-desktop-portal-gtk \
+xcb-proto \
+xutils-dev \
+xwayland
